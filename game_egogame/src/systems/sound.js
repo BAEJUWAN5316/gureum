28bd93a621a202ea514631351de38644c6991370
juwan.bae <vitr5316@naver.com>
2025-10-26 22:51:34 +0900

    egogame 그래픽 문제 수정3

--- game_egogame/src/systems/sound.js
+++ game_egogame/src/systems/sound.js
@@ -11,11 +11,11 @@ export class SoundManager {
         this.lastVolume = this.volume;
 
         this.soundPaths = {
-            jump: '/assets/sounds/jump.wav',
-            dialogue_next: '/assets/sounds/dialogue_next.wav',
-            recruit_success: '/assets/sounds/recruit_success.wav',
-            door_open: '/assets/sounds/door_open.wav',
-            background_music: '/assets/sounds/background_music.wav',
+            jump: '../assets/sounds/jump.wav',
+            dialogue_next: '../assets/sounds/dialogue_next.wav',
+            recruit_success: '../assets/sounds/recruit_success.wav',
+            door_open: '../assets/sounds/door_open.wav',
+            background_music: '../assets/sounds/background_music.wav',
         };
 
         this.bgmSource = null; // To store the background music AudioBufferSourceNode
