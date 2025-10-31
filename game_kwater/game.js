fbb1d83db05fc49a15c2605c100679bc32274782
juwan.bae <vitr5316@naver.com>
2025-10-31 18:53:44 +0900

    kwater 게임 디버깅15

--- game_kwater/game.js
+++ game_kwater/game.js
@@ -176,7 +176,7 @@ const player = {
     velocityX: 0,
     velocityY: 0,
     speed: 6,  // 이동 속도 (timeScale 적용됨, 1초에 픽셀 수)
-    jumpPower: 7,  // 점프력 증가 (높이 올림, 속도는 gravity로 조절)
+    jumpPower: 3,  // 점프력 증가 (높이 올림, 속도는 gravity로 조절)
     isJumping: false,
     isFalling: false,
     canJump: false,
